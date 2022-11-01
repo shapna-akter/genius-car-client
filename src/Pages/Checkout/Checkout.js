b0f9e7470ebe4686f4b6bb14f663ba22dc60129a
Shapna <shapna088@gmail.com>
2022-11-02 04:11:48 +0600

    added

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -3,9 +3,11 @@ import { useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 
 const Checkout = () => {
-    const {_id, title, price} = useLoaderData();
-    const {user} = useContext(AuthContext);
-    const handlePlaceOrder = event =>{
+    const { _id, title, price } = useLoaderData();
+    const { user } = useContext(AuthContext);
+
+
+    const handlePlaceOrder = event => {
         event.preventDefault();
         const form = event.target;
         const name = `${form.firstName.value} ${form.lastName.value}`;
@@ -23,8 +25,32 @@ const Checkout = () => {
             phone,
             message,
         }
+        // if(phone.length> 10){
+        //     alert("Phone number should be 10 character or longer")
+        // }
+
+        //server side a data send korar jonno post method use korbo
+        fetch('http://localhost:5000/orders', {
+            method: 'POST', // 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(order),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+                if(data.acknowledged){
+                    alert('Order Placed succecssfully')
+                    form.reset()
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
     }
-    
+
     return (
         <div>
             <form onSubmit={handlePlaceOrder}>
@@ -33,14 +59,14 @@ const Checkout = () => {
                 <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
                     <input name="firstName" type="text" placeholder="First name" className="input input-ghost w-full input-bordered" />
                     <input name="lastName" type="text" placeholder="Last name" className="input input-ghost w-full input-bordered" />
-                    <input name="phone" type="text" placeholder="Your Phone" className="input input-ghost w-full input-bordered" />
-                    <input name="email" type="text" placeholder="Your Email" defaultValue={user?.email} className="input input-ghost w-full input-bordered" readOnly/>
+                    <input name="phone" type="text" placeholder="Your Phone" className="input input-ghost w-full input-bordered" required />
+                    <input name="email" type="text" placeholder="Your Email" defaultValue={user?.email} className="input input-ghost w-full input-bordered" readOnly />
                 </div>
-                <textarea name="message" className="textarea textarea-bordered h-24 w-full" placeholder="Your message"></textarea>
+                <textarea name="message" className="textarea textarea-bordered h-24 w-full" placeholder="Your message" required></textarea>
                 <input className='btn' type="submit" value="Place your order" />
             </form>
         </div>
-    ); 
+    );
 };
 
 export default Checkout;
