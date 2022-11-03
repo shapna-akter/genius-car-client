11d9f62088996aa1f5bfb4def152c5f2a96a6fe3
Shapna <shapna088@gmail.com>
2022-11-04 01:49:05 +0600

    added

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
             },
             {
                 path: '/orders',
-                element: <Orders></Orders>,
+                element: <PrivateRoute><Orders></Orders></PrivateRoute>,
             }
         ]
     }
