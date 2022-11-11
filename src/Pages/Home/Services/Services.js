baa732a1ae727a56cd689bde09ccfb750adb494d
Shapna <shapna088@gmail.com>
2022-11-11 19:34:39 +0600

    added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -9,7 +9,7 @@ const Services = () => {
     const searchRef = useRef();
 
     useEffect(() => {
-        fetch(`http://localhost:5000/services?search=${search}&order=${ isAsc ? 'asc' : 'desc'}`)
+        fetch(`https://genius-car-server-omega.vercel.app/services?search=${search}&order=${ isAsc ? 'asc' : 'desc'}`)
             .then(res => res.json())
             .then(data => setServices(data))
     }, [isAsc, search])
